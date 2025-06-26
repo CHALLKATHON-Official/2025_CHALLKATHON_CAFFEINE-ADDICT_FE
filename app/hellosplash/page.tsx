46f1081c2f91ed59f67991ad9a30a15fcd7b8fc7
nayeong lee <lny021102@gmail.com>
2025-06-26 14:18:06 +0900

    fix: 랜딩페이지 디자인 수정

--- app/hellosplash/page.tsx
+++ app/hellosplash/page.tsx
@@ -54,13 +54,34 @@ export default function HelloSplash() {
 			display: 'flex',
 			flexDirection: 'column',
 			alignItems: 'center',
-			justifyContent: 'flex-end',
-			backgroundImage: `url('/img/firstSplash_backImg.svg')`,
-			backgroundSize: 'cover',
-			backgroundRepeat: 'no-repeat',
-			backgroundPosition: 'center',
-			padding: '3rem 0 4rem 0'
+			justifyContent: 'center',
+			backgroundColor: '#FFFDEB',
+			padding: '3rem 0 4rem 0',
+			gap: '17rem'
 		}}>
+			<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '2rem', alignItems: 'center', justifyContent: 'center' }}>
+				<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center', justifyContent: 'center' }}>
+					<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '0.5rem', alignItems: 'center', justifyContent: 'center' }}>
+						<Typography sx={{ color: '#B49066', fontSize: '0.8rem' }}>매일 스쳐 지나가는 하루,</Typography>
+						<Typography sx={{ color: '#B49066', fontSize: '0.8rem' }}>그 속에도 가족의 마음이 머무는 순간이 있습니다.</Typography>
+					</Box>
+
+					<Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '0.5rem', alignItems: 'center', justifyContent: 'center' }}>
+						<Typography sx={{ color: '#B49066', fontSize: '0.8rem' }}>오늘도 함께 나눈 대화 한 줄이,</Typography>
+						<Typography sx={{ color: '#B49066', fontSize: '0.8rem' }}>내일의 추억이 되기를 바랍니다.</Typography>
+					</Box>
+				</Box>
+
+				<Box sx={{
+					width: '10rem',
+					height: '2.5rem',
+					backgroundImage: `url(/img/logo_for_landing.svg)`,
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
+					backgroundPosition: 'center',
+				}} />
+			</Box>
+
 			<Button
 				onClick={handleKakaoLogin}
 				sx={{
